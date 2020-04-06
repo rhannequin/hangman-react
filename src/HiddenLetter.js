148816a0098e66d0eafc8c7dbe5711195299c9be
rhannequin <remy@getaround.com>
2020-04-06 19:23:34 +0200

    Hide hidden word's letters by default

--- src/HiddenLetter.js
+++ src/HiddenLetter.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import './HiddenLetter.css'
 
-const HiddenLetter = ({ value }) => (
-  <span className="hidden-letter">{value}</span>
-)
+const DEFAULT_STATUS = 'unknown'
+const DEFAULT_VALUE = "_"
+
+class HiddenLetter extends Component {
+  state = {
+    status: DEFAULT_STATUS,
+  }
+
+  render() {
+    const { value } = this.props
+    const { status } = this.state
+
+    return (
+      <span className="hidden-letter">
+        {status !== DEFAULT_STATUS ? value : DEFAULT_VALUE}
+      </span>
+    )
+  }
+}
 
 export default HiddenLetter
