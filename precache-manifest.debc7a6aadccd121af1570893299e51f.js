822acc6ccec1113e05f6c22bfc4da3004489568e
rhannequin <remy@getaround.com>
2020-04-06 19:58:56 +0200

    Updates

---
+++ precache-manifest.debc7a6aadccd121af1570893299e51f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2125f90e1e018db832952f844089ff39",
+    "url": "/hangman-react/index.html"
+  },
+  {
+    "revision": "67f358401f55d700713e",
+    "url": "/hangman-react/static/css/main.3e8c1bf4.chunk.css"
+  },
+  {
+    "revision": "ae27aad5f036f57a3417",
+    "url": "/hangman-react/static/js/2.43af3fa7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hangman-react/static/js/2.43af3fa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67f358401f55d700713e",
+    "url": "/hangman-react/static/js/main.d721634f.chunk.js"
+  },
+  {
+    "revision": "9ed35ac3965ee60012f4",
+    "url": "/hangman-react/static/js/runtime-main.6f31b2b1.js"
+  }
+]);
