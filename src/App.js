a689741da15a789a02d73de9cb8d592537dbbe29
rhannequin <remy@getaround.com>
2020-04-07 17:09:42 +0200

    Update App state when clicking on letter

--- src/App.js
+++ src/App.js
@@ -5,16 +5,29 @@ import HiddenWord from "./HiddenWord"
 import Keyboard from "./Keyboard"
 
 class App extends Component {
-  handleLetterClick(letter) {
-    console.log("Clicked on keyboard letter", letter)
+  state = {
+    foundLetters: [],
+  }
+
+  // Arrow fx for binding
+  handleLetterClick = (letter) => {
+    if(!this.state.foundLetters.includes(letter)) {
+      this.setState(
+        (prevState, _) => (
+          { foundLetters: [...prevState.foundLetters, letter] }
+        )
+      )
+    }
   }
 
   render() {
+    const { foundLetters } = this.state
+
     return (
       <div className="App">
         <div className="ui container">
           <h1 className="ui header">Hangman React</h1>
-          <HiddenWord foundLetters={[]} />
+          <HiddenWord foundLetters={foundLetters} />
           <Keyboard handleLetterClick={this.handleLetterClick} />
         </div>
       </div>
