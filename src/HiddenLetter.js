647dee04d705e5553735603ec97cce6a1c7bc1b6
rhannequin <remy@getaround.com>
2020-04-08 01:32:23 +0200

    Improve efficiency in HiddenWord

--- src/HiddenLetter.js
+++ src/HiddenLetter.js
@@ -2,15 +2,12 @@ import React from "react"
 
 import "./HiddenLetter.css"
 
-const DEFAULT_STATUS = "unknown"
 const DEFAULT_VALUE = "_"
 
-const HiddenLetter = ({ value, status }) => {
-  return (
-    <span className="hidden-letter">
-      {status !== DEFAULT_STATUS ? value : DEFAULT_VALUE}
-    </span>
-  )
-}
+const HiddenLetter = ({ value, reveal }) => (
+  <span className="hidden-letter">
+    {reveal ? value : DEFAULT_VALUE}
+  </span>
+)
 
 export default HiddenLetter
--- src/HiddenWord.js
+++ src/HiddenWord.js
@@ -1,32 +1,19 @@
-import React, { Component } from "react"
+import React from "react"
 
 import "./HiddenWord.css"
 
 import HiddenLetter from "./HiddenLetter"
 
-const STATUS_FOUND = "found"
-const STATUS_UNKNOWN = "unknown"
-
-class HiddenWord extends Component {
-  statusForLetter(letter) {
-    return this.props.selectedLetters.includes(letter) ? STATUS_FOUND : STATUS_UNKNOWN
-  }
-
-  render() {
-    const { lettersToGuess } = this.props
-
-    return (
-      <div className="ui segment">
-        {lettersToGuess.map((letter, index) => (
-          <HiddenLetter
-            key={index}
-            value={letter}
-            status={this.statusForLetter(letter)}
-          />
-        ))}
-      </div>
-    )
-  }
-}
+const HiddenWord = ({ lettersToGuess, selectedLetters }) => (
+  <div className="ui segment">
+    {lettersToGuess.map((letter, index) => (
+      <HiddenLetter
+        key={index}
+        value={letter}
+        reveal={selectedLetters.includes(letter)}
+      />
+    ))}
+  </div>
+)
 
 export default HiddenWord
